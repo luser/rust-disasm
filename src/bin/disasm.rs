f02fb6cb6859da0cd31cd8d89c7326a2fba7cd14
Ted Mielczarek <ted@mielczarek.org>
2018-03-15 06:36:39 -0400

    refactor things to provide a disasm_bytes function

--- src/bin/disasm.rs
+++ src/bin/disasm.rs
@@ -3,7 +3,7 @@ extern crate disasm;
 use std::env;
 
 fn main() {
-    match disasm::disasm(env::args_os().nth(1).unwrap()) {
+    match disasm::disasm_file(env::args_os().nth(1).unwrap()) {
         Ok(_) => {},
         Err(e) => eprintln!("{}", e),
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
 extern crate addr2line;
 extern crate capstone;
+extern crate gimli;
 extern crate moria;
 extern crate object;
 extern crate memmap;
 
 #[macro_use] extern crate failure;
 
-use addr2line::Location;
+use addr2line::{Context, Location};
 use capstone::{Arch, Capstone, NO_EXTRA_MODE, Mode};
 use failure::Error;
 use object::{Machine, Object, ObjectSection, SectionKind};
@@ -28,8 +29,15 @@ pub enum DisasmError {
     Object { reason: &'static str },
 }
 
+#[derive(Copy, Clone, Debug, PartialEq)]
+#[allow(non_camel_case_types)]
+pub enum CpuArch {
+    X86,
+    X86_64,
+}
+
 #[derive(Debug, PartialEq)]
-struct SourceLocation {
+pub struct SourceLocation {
     file: PathBuf,
     line: u64,
 }
@@ -37,6 +45,28 @@ struct SourceLocation {
 impl<'a> PartialEq<(&'a Path, u64)> for SourceLocation {
     fn eq(&self, other: &(&'a Path, u64)) -> bool {
         return self.file == other.0 && self.line == other.1;
+    }
+}
+
+pub trait SourceLookup {
+    fn lookup(&mut self, address: u64) -> Option<SourceLocation>;
+}
+
+impl<R> SourceLookup for Context<R>
+    where
+    R: gimli::Reader + Sync,
+    R::Offset: Sync,
+{
+    fn lookup(&mut self, address: u64) -> Option<SourceLocation> {
+        self.find_location(address).ok()
+            .and_then(|loc| loc)
+            .and_then(|Location { file, line, .. }| {
+                if let (Some(file), Some(line)) = (file, line) {
+                    Some(SourceLocation { file, line })
+                } else {
+                    None
+                }
+            })
     }
 }
 
@@ -66,74 +96,85 @@ fn print_source_line(loc: &SourceLocation, source_lines: &mut HashMap<PathBuf, O
     Ok(())
 }
 
-fn disasm_sections<'a>(obj: &object::File<'a>, debug_obj: &object::File<'a>) -> Result<(), Error> {
-    let (arch, mode) = match obj.machine() {
-        Machine::X86 => (Arch::X86, Mode::Mode32),
-        Machine::X86_64 => (Arch::X86, Mode::Mode64),
-        _ => unimplemented!(),
+pub fn disasm_bytes(bytes: &[u8],
+                    base_address: u64,
+                    arch: CpuArch,
+                    lookup: &mut SourceLookup) -> Result<(), Error> {
+    let (arch, mode) = match arch {
+        CpuArch::X86 => (Arch::X86, Mode::Mode32),
+        CpuArch::X86_64 => (Arch::X86, Mode::Mode64),
     };
-    let map = addr2line::Context::new(debug_obj).or(Err(DisasmError::Addr2Line))?;
     let mut source_lines = HashMap::new();
+    let cs = Capstone::new_raw(arch, mode, NO_EXTRA_MODE, None)?;
+    let mut last_loc: Option<SourceLocation> = None;
+    for i in cs.disasm_all(bytes, base_address)?.iter() {
+        let loc = lookup.lookup(i.address());
+        if let Some(loc) = loc {
+            let this_loc = loc;
+            match last_loc {
+                None => {
+                    println!("{}", this_loc.file.to_string_lossy());
+                    print_source_line(&this_loc, &mut source_lines)?;
+                }
+                Some(ref last) => {
+                    if last.file != this_loc.file {
+                        println!("{}", this_loc.file.to_string_lossy());
+                    }
+                    if last.line != this_loc.line {
+                        print_source_line(&this_loc, &mut source_lines)?;
+                    }
+
+                }
+            }
+            last_loc = Some(this_loc);
+        } else {
+            last_loc = None;
+        }
+        println!("{}", i);
+    }
+    println!("");
+    Ok(())
+}
+
+fn disasm_text_sections<'a>(obj: &object::File<'a>,
+                            debug_obj: &object::File<'a>) -> Result<(), Error> {
+    let mut map = Context::new(debug_obj).or(Err(DisasmError::Addr2Line))?;
+    let arch = match obj.machine() {
+        Machine::X86 => CpuArch::X86,
+        Machine::X86_64 => CpuArch::X86_64,
+        a @ _ => return Err(format_err!("Unsupported CPU architecture {:?}", a)),
+    };
     for sect in obj.sections() {
         let name = sect.name().unwrap_or("<unknown>");
         if sect.kind() == SectionKind::Text {
             println!("Disassembly of section {}:", name);
-            let cs = Capstone::new_raw(arch, mode, NO_EXTRA_MODE, None)?;
-            let mut last_loc: Option<SourceLocation> = None;
-            for i in cs.disasm_all(sect.data(), sect.address())?.iter() {
-                let loc = map.find_location(i.address()).or(Err(DisasmError::Addr2Line))?;
-                if let Some(Location { file: Some(file), line: Some(line), .. }) = loc {
-                    let this_loc = SourceLocation { file, line };
-                    match last_loc {
-                        None => {
-                            println!("{}", this_loc.file.to_string_lossy());
-                            print_source_line(&this_loc, &mut source_lines)?;
-                        }
-                        Some(ref last) => {
-                            if last.file != this_loc.file {
-                                println!("{}", this_loc.file.to_string_lossy());
-                            }
-                            if last.line != this_loc.line {
-                                print_source_line(&this_loc, &mut source_lines)?;
-                            }
-
-                        }
-                    }
-                    last_loc = Some(this_loc);
-                } else {
-                    last_loc = None;
-                }
-                println!("{}", i);
-            }
-            println!("");
+            disasm_bytes(sect.data(), sect.address(), arch, &mut map)?;
         }
     }
     Ok(())
 }
 
-pub fn disasm<P>(path: P) -> Result<(), Error>
+fn with_file<F>(path: &Path, func: F) -> Result<(), Error>
+    where F: Fn(&object::File) -> Result<(), Error>
+{
+    let f = File::open(path)?;
+    let buf = unsafe { memmap::Mmap::map(&f)? };
+    let obj = object::File::parse(&*buf).map_err(|e| DisasmError::Object { reason: e } )?;
+    func(&obj)
+}
+
+pub fn disasm_file<P>(path: P) -> Result<(), Error>
     where P: AsRef<Path>,
 {
     let path = path.as_ref();
-    let f = File::open(path)?;
-    let buf = unsafe { memmap::Mmap::map(&f)? };
-
-    let obj = object::File::parse(&*buf).map_err(|e| DisasmError::Object { reason: e } )?;
-    if obj.has_debug_symbols() {
-        disasm_sections(&obj, &obj)
-    } else {
-        let debug_path = moria::locate_debug_symbols(&obj, path)?;
-        let debug_f = File::open(debug_path)?;
-        let debug_buf = unsafe { memmap::Mmap::map(&debug_f)? };
-        let debug_obj = object::File::parse(&*debug_buf).map_err(|e| DisasmError::Object { reason: e } )?;
-        disasm_sections(&obj, &debug_obj)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+    with_file(path, |obj| {
+        if obj.has_debug_symbols() {
+            disasm_text_sections(&obj, &obj)
+        } else {
+            let debug_file = moria::locate_debug_symbols(obj, path)?;
+            with_file(&debug_file, |debug_obj| {
+                disasm_text_sections(&obj, &debug_obj)
+            })
+        }
+    })
 }
